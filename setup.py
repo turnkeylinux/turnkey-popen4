bd1076218fdb479cf6d0fcbda691ca054a644cf9
Stefan Davis <stefan@turnkeylinux.org>
2019-06-26 01:54:06 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='turnkey-command',
+    version='1.0',
+    description='A popen4 implementation',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-popen4',
+    packages=['popen4'])
